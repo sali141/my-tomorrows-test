e324c67d788e4cff2fa339f0be948f28a63a1015
Saliya Randunu <saliya@limarktech.com>
2022-11-08 14:40:00 +0530

    Create initial components

---
+++ src/app/components/weather-report/weather-report.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-report',
+  templateUrl: './weather-report.component.html',
+  styleUrls: ['./weather-report.component.scss']
+})
+export class WeatherReportComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
